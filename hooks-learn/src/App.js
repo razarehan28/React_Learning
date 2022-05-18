4508d699dd2dc8abdd56759ec2927d9f76c63a18
razarehan28 <rehan.r@simformsolutions.com>
2022-05-18 10:40:41 +0530

    useState in Hooks

---
+++ hooks-learn/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ClassCounter from './component/ClassCounter'
+import HookCounter from './component/HookCounter';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ClassCounter /> */} 
+      <HookCounter />
+    </div>
+  );
+}
+
+export default App;
