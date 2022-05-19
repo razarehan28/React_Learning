69c888ea3234a3bce7eba89535cd9aee04169fb8
razarehan28 <rehan.r@simformsolutions.com>
2022-05-19 16:54:22 +0530

    useEffect Hooks,useContext Hooks

--- hooks-learn/src/App.js
+++ hooks-learn/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import ClassCounter from './component/ClassCounter'
-import HookCounter from './component/HookCounter';
-import HookCounterTwo from './component/HookCounterTwo';
-import HookCounterThree from './component/HookCounterThree';
-import HookCounterFour from './component/HookCounterFour';
-import HookCounterone from './component/UseEffect/HookCounterone';
-import HookMouse from './component/UseEffect/HookMouse';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ClassCounter from "./component/ClassCounter";
+import HookCounter from "./component/HookCounter";
+import HookCounterTwo from "./component/HookCounterTwo";
+import HookCounterThree from "./component/HookCounterThree";
+import HookCounterFour from "./component/HookCounterFour";
+import HookCounterone from "./component/UseEffect/HookCounterone";
+import HookMouse from "./component/UseEffect/HookMouse";
+import MouseContainer from "./component/UseEffect/MouseContainer";
+import DataFetching from "./component/UseEffect/DataFetching";
+import ComponentC from "./component/UseContext/ComponentC";
+export const UserContext = React.createContext();
+export const ChannelContext = React.createContext();
 
 function App() {
   return (
     <div className="App">
-      {/* <ClassCounter /> */} 
+      {/* <ClassCounter /> */}
       {/* <HookCounter /> */}
       {/* <HookCounterTwo /> */}
       {/* <HookCounterThree /> */}
       {/* <HookCounterFour /> */}
       {/* <HookCounterone /> */}
-    <HookMouse />
+      {/* <HookMouse /> */}
+      {/* <MouseContainer /> */}
+      {/* <DataFetching /> */}
+      <UserContext.Provider value={"Rehan"}>
+        <ChannelContext value={"Raza"}>
+          <ComponentC />
+        </ChannelContext>
+      </UserContext.Provider>
     </div>
   );
 }
---
+++ hooks-learn/src/component/UseEffect/DataFetching.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function DataFetching() {
+  const [post, setPost] = useState({});
+  const [id, setId] = useState(1);
+  const [IdFromButtonClick, setIdFromButtonClick] = useState(1);
+  const handleClick = () => {
+    setIdFromButtonClick(id);
+  };
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((res) => {
+        console.log(res);
+        setPost(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [IdFromButtonClick]); // id as a dependency
+  return (
+    <div>
+      <input
+        type="text"
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+      ></input>
+      <button type="button" onClick={handleClick}>
+        Fetch Post
+      </button>
+      <div>{post.title}</div>
+    </div>
+  );
+}
+
+export default DataFetching;
